e80a782d8d212c36c1031122afd690c4b40822ea
Eric Bernard <eric99b@gmail.com>
2022-04-03 16:16:58 -0400

    Proof of concept raycast attacks

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -43,7 +43,7 @@ public class Health : MonoBehaviour {
 		healthUI.SetLives(lives);
 	}
 
-	public void DealDamage(int damage) {
+	public void TakeDamage(int damage) {
 		health -= damage;
 		if (health < 0) {
 			OnDeath();
