e149ed7e9af65b93e175de521cd8b34840348b1d
Andrew <aanctil26@gmail.com>
2022-04-20 18:35:43 -0400

    Added Kick, Super, Doublepunch

---
+++ Assets/Scripts/AttackData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class AttackData {
+    public enum BasicAttackType {
+        LeftPunch,
+        RightPunch,
+        Kick,
+        DoublePunch,
+        Super
+    };
+
+    public static int getAttackDamage(BasicAttackType attackType) {
+        switch (attackType) {
+            default: {
+                return 7;
+            }
+            case BasicAttackType.Kick:
+            {
+                return 20;
+            }
+            case BasicAttackType.DoublePunch:
+            {
+                return 14;
+            }
+            case BasicAttackType.Super:
+            {
+                return 35;
+            }
+            
+        }
+    }
+
+    public static Vector2 getAttackDirection(BasicAttackType attackType) {
+        switch (attackType) {
+            default: {
+                return Vector2.right;
+            }
+        }
+    }
+}
