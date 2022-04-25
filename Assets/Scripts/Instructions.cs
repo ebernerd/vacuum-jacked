6a4a20c006774f7d3519d512e82c1c8dae1fbce9
ebernerd <eric99b@gmail.com>
2022-04-25 18:04:57 -0400

    Fix build-time errors

--- Assets/Scripts/Instructions.cs
+++ Assets/Scripts/Instructions.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro.EditorUtilities;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Instructions : MonoBehaviour
